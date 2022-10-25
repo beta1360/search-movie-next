1cf76f3f10a26a359cac29b22b448db2b73ef094
GeonHee Lee <beta1360@naver.com>
2022-10-25 17:43:28 +0900

    feat: Add Atomic Button

---
+++ components/atoms/Button.tsx
@@ -0,0 +1,19 @@
+type ButtonProps = {
+  label: string,
+  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick
+}) => {
+  return (
+    <button
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  )
+}
+
+export default Button
