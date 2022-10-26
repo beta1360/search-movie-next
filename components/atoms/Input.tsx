39eeb3c6e52aa6de5f05f3b99a7c6351dc409d52
GeonHee Lee <beta1360@naver.com>
2022-10-26 18:14:13 +0900

    feat: Add style in input

--- components/atoms/Input.tsx
+++ components/atoms/Input.tsx
@@ -2,9 +2,37 @@ import { useState } from 'react'
 import { css } from '@emotion/react'
 import { useCallback } from 'react'
 
-const inputWrapperStyle = css({
-  border: 'skyblue 1px solid'
-})
+const getInputWrapperStyle = (isFocus: boolean) => css`
+  display: flex;
+  align-items: center;
+  border: skyblue ${isFocus ? 2 : 1}px solid;
+  width: max-content;
+  padding: 5px 5px;
+  border-radius: 5px;
+  background-color: white;
+`
+
+const inputStyle = css`
+  border: 0;
+  font-size: 1.2rem;
+  padding: 5px 5px;
+  background-color: #fff;
+  color: #222;
+  &:focus {
+    outline: none;
+  }
+`
+
+const buttonStyle = css`
+  border: 0;
+  background: #fff;
+  width: 1.5rem;
+  font-size: 0.8rem;
+  color: skyblue;
+  padding: 0;
+  cursor: pointer;
+  margin-left: 8px;
+`
 
 type InputProps = {
   defaultInput?: string,
@@ -18,19 +46,35 @@ const BaseInput: React.FC<InputProps> = ({
   onChange
 }) => {
   const [value, setValue] = useState(defaultInput)
+  const [isFocus, setIsFocus] = useState(false)
 
   const clearInput = useCallback(() => {
     setValue('')
   }, [])
 
+  const onFocusInput = useCallback(() => {
+    setIsFocus(true)
+  }, [])
+
+  const onBlurInput = useCallback(() => {
+    setIsFocus(false)
+  }, [])
+
   return (
-    <div css={inputWrapperStyle}>
+    <div css={getInputWrapperStyle(isFocus)}>
       <input
         type="text"
+        css={inputStyle}
         defaultValue={value}
         onChange={onChange}
+        onFocus={onFocusInput}
+        onBlur={onBlurInput}
       />
-      { clearable && <button onClick={clearInput}>x</button> }
+      { clearable &&
+        <button
+          css={buttonStyle}
+          onClick={clearInput}
+        >x</button> }
     </div>
   )
 }
