226e0c916028325a009039abd594f29d4f89e30b
GeonHee Lee <beta1360@naver.com>
2022-11-08 10:46:19 +0900

    feat: Add QueryClient and Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,24 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { RecoilRoot } from 'recoil'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import BaseLayout from '@/components/layouts/BaseLayout'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient()
   return (
     <>
       <Head>
         <title>Marco Toyproject - search-movie-app</title>
       </Head>
       <div className="app__wrapper">
-        <RecoilRoot>
-          <BaseLayout>
-            <Component {...pageProps} />
-          </BaseLayout>
-        </RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <RecoilRoot>
+            <BaseLayout>
+              <Component {...pageProps} />
+            </BaseLayout>
+          </RecoilRoot>
+        </QueryClientProvider>
       </div>
     </>    
   )
